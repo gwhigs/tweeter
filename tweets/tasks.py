bab2056ea9e47eec1d0bd15f86ff9cf08f8dbafc
Graham Higgins <gwhiggins@gannett.com>
2017-03-12 12:14:13 -0400

    Use shared_task instead of deprecated periodic_task

--- tweets/tasks.py
+++ tweets/tasks.py
@@ -1,11 +1,8 @@
-from datetime import timedelta
-
-from django.conf import settings
 from django.core.management import call_command
 
-from celery.task import periodic_task
+from celery import shared_task
 
 
-@periodic_task(run_every=timedelta(minutes=settings.TWEET_INTERVAL_MINUTES))
+@shared_task()
 def tweet_next_task():
     call_command('tweet_next', verbose=True, interactive=False)
